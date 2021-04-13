3f5e44a887a7733ef8e388ad9f5e20a12cbc98f5
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2021-04-13 21:53:49 +0900

    Feat : Homework3

---
+++ practice1/HW3_홍수혁.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+library(dplyr)
+
+midwest <- as.data.frame(ggplot2::midwest)
+
+#1
+midwest <- 
+  midwest %>%
+  mutate(popnotadult = (poptotal - popadults)/poptotal*100)
+
+#2
+midwest %>%
+  arrange(desc(popnotadult)) %>%
+  select(county, popnotadult) %>%
+  head(5)
+
+#3
+midwest <- midwest %>% 
+  mutate(notadultSize = ifelse(popnotadult >= 40, "Large",
+                  ifelse(popnotadult >= 30, "Middle", "Small")))
+
+table(midwest$notadultSize)
+qplot(midwest$notadultSize)
+
+#4
+midwest %>%
+  mutate(poptotalasian = (popasian/poptotal)*100) %>%
+  select(state, county, poptotalasian) %>%
+  arrange(poptotalasian) %>%
+  tail(10)
